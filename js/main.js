e03b90b8c59c05d7a1ae2118a9eb69e3898a688f
Mahipat Gami <info.utilitymega@gmail.com>
2026-03-14 20:30:26 +0530

    minor

--- js/main.js
+++ js/main.js
@@ -5,21 +5,20 @@ gtagScript.src = "https://www.googletagmanager.com/gtag/js?id=G-HJ7NVQ7CMJ";
 gtagScript.async = true;
 document.head.appendChild(gtagScript);
 
-```
 window.dataLayer = window.dataLayer || [];
 function gtag(){window.dataLayer.push(arguments);}
 window.gtag = gtag;
 
 gtag('js', new Date());
 gtag('config', 'G-HJ7NVQ7CMJ');
-```
+
 
 })();
 
 // Wait for DOM before adding Vercel scripts
 document.addEventListener("DOMContentLoaded", function () {
 
-```
+
 // Vercel Insights
 var insights = document.createElement("script");
 insights.src = "/_vercel/insights/script.js";
@@ -31,7 +30,7 @@ var speedInsights = document.createElement("script");
 speedInsights.src = "/_vercel/speed-insights/script.js";
 speedInsights.defer = true;
 document.body.appendChild(speedInsights);
-```
+
 
 });
 s
